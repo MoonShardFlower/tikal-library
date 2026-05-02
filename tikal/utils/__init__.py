355bddfd18e1e20cf2bab9aa6602edddf9c92afd
Robin-Sonner <MoonShardFlower@gmail.com>
2026-05-02 19:55:41 +0200

    Export abstract class "Transport"

--- tikal/utils/__init__.py
+++ tikal/utils/__init__.py
@@ -1,5 +1,5 @@
 from .async_runner import AsyncRunner
 from .pattern_handler import PatternHandler
-from .transport import BleTransport, UsbTransport
+from .transport import BleTransport, Transport, UsbTransport
 
-__all__ = ["AsyncRunner", "BleTransport", "UsbTransport", "PatternHandler"]
+__all__ = ["AsyncRunner", "BleTransport", "UsbTransport", "Transport", "PatternHandler"]
