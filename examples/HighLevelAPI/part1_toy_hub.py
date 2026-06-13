dae4cf4d767bfe7f45b47aea25edae8198788c59
MoonShardFlower <MoonShardFlower@gmail.com>
2026-06-13 21:08:30 +0200

    small bugfix (BATTERY_UPDATE_INTERVAL is no longer part of the ToyHub class)

--- examples/HighLevelAPI/part1_toy_hub.py
+++ examples/HighLevelAPI/part1_toy_hub.py
@@ -213,9 +213,7 @@ def main():
     )
     # Setting/Updating a callback can be done like this:
     new_toy_hub.battery_update_callback(on_battery_update)
-    print(
-        f"The battery callback will be called every {new_toy_hub.BATTERY_UPDATE_INTERVAL} seconds"
-    )
+    print(f"The battery callback will be called every 120 seconds")
 
     # Connecting new toys will trigger the battery update callback immediately.
     MockBleakScanner.reset()  # you obviously don't need to do this with the real BleakScanner
