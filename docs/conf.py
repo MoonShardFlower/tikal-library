455424c7397073e3b0a0ca1bbf665ba851beeaba
MoonShardFlower <MoonShardFlower@gmail.com>
2026-07-10 14:09:57 +0200

    Updated to version 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "TIKAL"
 copyright = "2026, SnugglyBound"
 author = "SnugglyBound"
-release = "1.1.0"
+release = "1.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
